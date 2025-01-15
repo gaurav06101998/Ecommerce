c5052e65a1d07caf424e6fec8718e0cfadc59483
Gaurav singh <78979515+gaurav06101998@users.noreply.github.com>
2025-01-15 10:51:40 +0530

    first deploy

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,4 +1,6 @@
+import dj_database_url
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,12 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5gj-in@z=^7y5s5qc=a#!(!%cn($!9lvs#@938yx)vsd9h9leh'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 LOGGING = {
     'version': 1,
@@ -112,6 +114,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+database_url = os.environ.get('DATABASES_URL')
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 
 # Password validation
