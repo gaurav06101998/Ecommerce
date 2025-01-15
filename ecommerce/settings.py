69a2b920137748dceb3cccb511c260881dafbada
Gaurav singh <78979515+gaurav06101998@users.noreply.github.com>
2025-01-15 14:40:25 +0530

    correcting the path for the static files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -111,12 +111,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-database_url = os.environ.get('DATABASES_URL')
-DATABASES['default'] = dj_database_url.parse(database_url)
+# database_url = os.environ.get('DATABASES_URL')
+# DATABASES['default'] = dj_database_url.parse(database_url)
 
-# DATABASES = {
-#     'default': dj_database_url.parse(config('DATABASES_URL', default=''), conn_max_age=600, ssl_require=True)
-# }
+DATABASES = {
+    'default': dj_database_url.parse(config('DATABASES_URL', default=''), conn_max_age=600, ssl_require=True)
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -153,7 +153,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'shop/static',  # Path to your static directory
+    BASE_DIR / 'shop/templates/shop/static',  # Path to your static directory
+    BASE_DIR / 'shop/templates/admin',  # Path to your static directory
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
